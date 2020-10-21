515e262a9c1c312c8f6058b7a6834d1ec9a8e3ba
kuworking <kuworking@gmail.com>
2020-10-21 11:59:04 +0200

    simplify webpack

--- plugins/gutenbloqs-imgur/webpack.config.js
+++ plugins/gutenbloqs-imgur/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const defaults = require('@wordpress/scripts/config/webpack.config')
-const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
   ...defaults,
@@ -11,47 +10,5 @@ module.exports = {
   externals: {
     react: 'React',
     'react-dom': 'ReactDOM',
-  },
-  module: {
-    ...defaults.module,
-    rules: [
-      ...defaults.module.rules,
-      {
-        test: /\.js$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
-            plugins: ['emotion'],
-            //            plugins: ['@babel/plugin-transform-modules-commonjs'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: require.resolve('css-loader'),
-            options: {
-              sourceMap: !isProduction,
-            },
-          },
-        ],
-      },
-      /*
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'files/',
-            },
-          },
-        ],
-      },
-      */
-    ],
   },
 }
